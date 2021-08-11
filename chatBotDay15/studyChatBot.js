a54b5607f4b24b92aa434c24c5dfdca60f7e0ee2
SemiKimm <semikimm.m@gmail.com>
2021-08-12 00:23:43 +0900

    모각코 Web 스터디 챗봇만들기 - 15일차
    챗봇에게 말을 가르치는 기능 구현

---
+++ chatBotDay15/studyChatBot.js
@@ -0,0 +1,135 @@
+const orderInput = document.querySelector('.orderInput');
+const orderBtn = document.querySelector('.btn');
+const answerBox = document.querySelector('.answer');
+let count = 0;
+let question = '';
+let saveAnswer = '';
+let key = 0;
+const orderSet = [
+  '안녕',
+  '공부대신해줄래?',
+  '나랑놀자',
+  '불꺼주라',
+  '오늘날씨어때?',
+  '119불러줘!',
+];
+const answerSet = [
+  {
+    order: '안녕',
+    answer: '반갑다냥',
+    colorBack: '#ffffff',
+    colorAsBox: '#000000',
+  },
+  {
+    order: '공부대신해줄래?',
+    answer: '네 공부는 네가 해야지냥',
+    colorBack: '#ffffff',
+    colorAsBox: '#000000',
+  },
+  {
+    order: '나랑놀자',
+    answer: '좋아냥! 뭐하고 놀까냥??',
+    colorBack: '#ffffff',
+    colorAsBox: '#000000',
+  },
+  {
+    order: '불꺼주라',
+    answer: '알겠다냥',
+    colorBack: '#000000',
+    colorAsBox: '#ffffff',
+  },
+  {
+    order: '오늘날씨어때?',
+    answer: '잘모르겠다냥..날씨정보를 보고오겠다냥',
+    colorBack: '#ffffff',
+    colorAsBox: '#000000',
+  },
+  {
+    order: '119불러줘!',
+    answer: '사건발생했다냥!긴급!!',
+    colorBack: '#ff0000',
+    colorAsBox: '#ffffff',
+  },
+];
+
+function initInputBox() {
+  orderInput.value = '';
+}
+
+function filterAnswer(data) {
+  const answerFilter = answerSet.filter((obj) => obj.order === data);
+  return answerFilter;
+}
+
+function getInputData() {
+  const inputData = orderInput.value;
+  initInputBox();
+  return inputData;
+}
+function changeBackground(answerD) {
+  document.querySelector('.body').style.backgroundColor = answerD[0].colorBack;
+  answerBox.style.color = answerD[0].colorAsBox;
+}
+function defaultBack() {
+  document.querySelector('.body').style.backgroundColor = '#ffffff';
+  answerBox.style.color = '#000000';
+}
+
+function countOrder1(orderD) {
+  if (orderD === '안녕') {
+    if (count == 0) {
+      count++;
+      answerBox.innerHTML = '반갑다냥';
+    } else if (count == 1) {
+      count++;
+      answerBox.innerHTML = '아까 인사했다냥';
+      document.querySelector('.body').style.backgroundColor = '#ffff00';
+    } else {
+      answerBox.innerHTML = '계속 인사했는데 왜그러냥!';
+      document.querySelector('.body').style.backgroundColor = '#ff0000';
+    }
+  }
+  console.log(count);
+}
+function pushAnswer() {
+  orderSet.push(`${question}`);
+  answerSet.push({ order: `${question}`, answer: `${saveAnswer}` });
+  answerBox.innerHTML = '말을 배웠다냥';
+  console.log(orderSet);
+  key = 0;
+}
+function pushAnswerMain() {
+  const orderData = getInputData();
+  const orderFilter = orderSet.filter((obj) => obj === orderData);
+  console.log(orderFilter);
+  if (key == 1) {
+    if (orderData == '응') {
+      answerBox.innerHTML = '가르칠 대답을 입력하세요';
+      key = 2;
+    } else {
+      answerBox.innerHTML = '뭐라는지 모르겠다냥';
+      key = 0;
+    }
+    return;
+  }
+  if (key == 2) {
+    saveAnswer = orderData;
+    window.addEventListener('keydown', (e) => {
+      if (e.keyCode == 13) pushAnswer();
+    });
+  }
+  if (orderFilter[0] === orderData) {
+    const answerF = filterAnswer(orderData);
+    console.log(answerF);
+    answerBox.innerHTML = answerF[0].answer;
+    changeBackground(answerF);
+    countOrder1(orderData);
+  } else {
+    defaultBack();
+    answerBox.innerHTML = '말 가르쳐주라냥(응/아니)';
+    question = orderData;
+    key = 1;
+  }
+}
+
+orderBtn.addEventListener('click', pushAnswerMain);
